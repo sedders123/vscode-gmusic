631fdad1bfd024e40bbe929676b39fe47868d030
James Seden Smith <sedders123@gmail.com>
2019-05-26 20:47:11 +0100

    Add status bar items
    Update dependencies
    
    Add Cycle Repeat Mode command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem } from 'vscode';
 import * as WebSocket from 'ws';
+import { IKeyedCollection, KeyedCollection } from "./utils"
 
 export function activate(context: ExtensionContext) {
     let gMusic = new gMusicClass(context);
@@ -32,11 +33,15 @@ export function activate(context: ExtensionContext) {
                         break;
                 }
             }
-        );
+            );
     });
     let restartCommand = commands.registerCommand('gmusic.restart', () => {
         gMusic.dispose();
         gMusic = new gMusicClass(context);
+    })
+
+    let cycleRepeatCommand = commands.registerCommand('gmusic.cycleRepeat', () => {
+        gMusic.cycleRepeat();
     })
 
     context.subscriptions.push(playpauseCommand);
@@ -44,10 +49,12 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(skipCommand);
     context.subscriptions.push(rewindCommand);
     context.subscriptions.push(likeCommand);
+    context.subscriptions.push(restartCommand)
+    context.subscriptions.push(cycleRepeatCommand)
     context.subscriptions.push(gMusic);
 }
 
-interface track {
+interface Track {
     title: string;
     artist: string;
     album: string;
@@ -59,9 +66,25 @@ interface gMusicResponse {
     payload: any;
 }
 
-interface rating {
+interface Rating {
     liked: boolean;
     disliked: boolean;
+}
+
+enum RepeatMode {
+    None = "NO_REPEAT",
+    Playlist = "LIST_REPEAT",
+    Song = "SINGLE_REPEAT"
+}
+
+interface Button {
+    id: string;
+    title: string;
+    command: string;
+    text: string;
+    dynamicText?: (cond: boolean) => string;
+    statusBarItem: StatusBarItem;
+    isVisible: boolean;
 }
 
 /**
@@ -70,23 +93,27 @@ interface rating {
  * @export
  * @class gMusicData
  */
-export class gMusicClass {
-    private _statusBarItem: StatusBarItem;
+export class gMusicClass implements Disposable {
+    private _nowPlayingStatusBarItem: StatusBarItem;
+    private _repeatStatusBarItem: StatusBarItem;
+    private _buttons: IKeyedCollection<Button> = new KeyedCollection<Button>();
 
     private _playState: boolean;
-    private _track: track;
-    private _rating: rating;
+    private _track: Track;
+    private _rating: Rating;
     private _shuffle: string;
-    private _repeat: string;
-    private _onChange: any;
-    private ws: any;
+    private _repeat: RepeatMode;
+    private ws: WebSocket;
 
     constructor(context: ExtensionContext) {
         const Cache = require('vscode-cache');
 
         // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        if (!this._nowPlayingStatusBarItem) {
+            this._nowPlayingStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+        if (this._buttons.Count() === 0) {
+            this.createControlButtons();
         }
 
         this.ws = new WebSocket('ws://localhost:5672');
@@ -111,7 +138,7 @@ export class gMusicClass {
 
         // Receiving data from GPMDP.
         this.ws.on('message', (data) => {
-            let gMusicResponse: gMusicResponse = JSON.parse(data);
+            let gMusicResponse: gMusicResponse = JSON.parse(data.toString());
             switch (gMusicResponse.channel) {
                 case 'connect':
                     if (gMusicResponse.payload === 'CODE_REQUIRED') {
@@ -120,18 +147,20 @@ export class gMusicClass {
                                 namespace: 'connect',
                                 method: 'connect',
                                 arguments: ['vscode-gmusic', code]
-                                }))
-                            })
-                        } else {
-                            codeCache.put('authCode', gMusicResponse.payload)
+                            }))
+                        })
+                    } else {
+                        codeCache.put('authCode', gMusicResponse.payload)
                     }
                     break;
                 case 'playState':
                     this._playState = gMusicResponse.payload;
+                    this.refreshNowPlaying();
+                    this.updateDynamicButton('playpause', this._playState);
                     break;
                 case 'track':
                     this._track = gMusicResponse.payload;
-                    this.refreshStatusBar();
+                    this.refreshNowPlaying();
                     break;
                 case 'rating':
                     this._rating = gMusicResponse.payload;
@@ -141,6 +170,7 @@ export class gMusicClass {
                     break;
                 case 'repeat':
                     this._repeat = gMusicResponse.payload;
+                    this.updateRepeatButtonState();
                     break;
             }
         });
@@ -148,10 +178,70 @@ export class gMusicClass {
         this.ws.on('error', (err) => window.showErrorMessage(`GMusic: WebSocket failed to connect`));
     }
 
-    public refreshStatusBar() {
-        let textItem = this._track ? '$(triangle-right) ' + this._track.title + ' - ' + this._track.artist : '$(primitive-square)'
-        this._statusBarItem.text = textItem
-        this._statusBarItem.show();
+    private createControlButtons() {
+        const buttons = [
+            { id: "rewind", title: "Previous Song", text: "$(chevron-left)" },
+            { id: "playpause", title: "Play / Pause", text: '$(triangle-right)', dynamicText: (currentlyPlaying: boolean) => currentlyPlaying ? '$(primitive-square)' : '$(triangle-right)' },
+            { id: "skip", title: "Next Song", text: "$(chevron-right)" }
+        ];
+
+        buttons.map(button => {
+            const command = "gmusic." + button.id;
+            const isVisible = true;
+            var statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+            statusBarItem.text = button.text;
+            statusBarItem.command = command;
+            statusBarItem.tooltip = button.title;
+            this._buttons.Add(button.id, Object.assign({}, button, { command, statusBarItem, isVisible }));
+            if (isVisible) statusBarItem.show();
+        });
+
+        this._repeatStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        this._repeatStatusBarItem.command = "gmusic.cycleRepeat"
+        this.updateRepeatButtonState();
+        this._repeatStatusBarItem.show()
+    }
+
+    private updateRepeatButtonState() {
+        switch (this._repeat) {
+            case RepeatMode.None:
+                this._repeatStatusBarItem.text = "$(sync)";
+                this._repeatStatusBarItem.color = "darkGrey";
+                this._repeatStatusBarItem.tooltip = "Not Repeating";
+                break;
+            case RepeatMode.Playlist:
+                this._repeatStatusBarItem.text = "$(sync)";
+                this._repeatStatusBarItem.color = "white";
+                this._repeatStatusBarItem.tooltip = "Repeating Playlist";
+                break;
+            case RepeatMode.Song:
+                this._repeatStatusBarItem.text = "$(issue-reopened)"
+                this._repeatStatusBarItem.color = "white";
+                this._repeatStatusBarItem.tooltip = "Repeating Song";
+                break;
+        }
+    }
+
+    private updateDynamicButton(id: string, condition: boolean) {
+        const button = this._buttons.Item(id);
+        const text = button.dynamicText(condition);
+        button.statusBarItem.text = text;
+    }
+
+    public refreshNowPlaying() {
+        let textItem = this.getNowPlayingText(this._track)
+        if (textItem == null) {
+            this._nowPlayingStatusBarItem.hide()
+        }
+        this._nowPlayingStatusBarItem.text = textItem
+        this._nowPlayingStatusBarItem.show();
+    }
+
+    private getNowPlayingText(track: Track): string {
+        if (track == null || track.title === null) {
+            return null;
+        }
+        return `${track.title} - ${track.artist}`
     }
 
     public togglePlay() {
@@ -186,11 +276,25 @@ export class gMusicClass {
         }))
     }
 
+    public cycleRepeat() {
+        switch (this._repeat) {
+            case RepeatMode.None:
+                this.toggleRepeat(RepeatMode.Playlist);
+                break;
+            case RepeatMode.Playlist:
+                this.toggleRepeat(RepeatMode.Song);
+                break;
+            case RepeatMode.Song:
+                this.toggleRepeat(RepeatMode.None);
+                break;
+        }
+    }
+
     public toggleRepeat(mode: string) {
         this.ws.send(JSON.stringify({
             namespace: 'playback',
             method: 'setRepeat',
-            arguments: mode
+            arguments: [mode]
         }))
     }
 
@@ -223,6 +327,17 @@ export class gMusicClass {
     }
 
     public dispose() {
-        this._statusBarItem.dispose();
+        this._nowPlayingStatusBarItem.dispose();
+        this._buttons.Values().forEach(button => {
+            button.statusBarItem.dispose();
+        });
+        this._repeatStatusBarItem.dispose();
+        this.ws.close();
+        process.nextTick(() => {
+            if ([this.ws.OPEN, this.ws.CLOSING].includes(this.ws.readyState)) {
+                // Socket still hangs, hard close
+                this.ws.terminate();
+            }
+        });
     }
 }
